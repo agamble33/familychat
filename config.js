47b22ddef8678347fcff4be22313dd1827f5a60c
Anthony S. Gamble <agamble25@gmail.com>
2018-09-30 13:12:32 -0500

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/bff8ef9d-5884-4ffe-925e-87ad7ac77987/token";
+const instanceLocator = "v1:us1:bff8ef9d-5884-4ffe-925e-87ad7ac77987";
+
+exports.tokenUrl = tokenUrl;
+exports.instanceLocator = instanceLocator;
+
